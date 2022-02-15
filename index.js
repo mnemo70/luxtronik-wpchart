c5fcbdf98aa20feb062330787d00fafe22b3292e
Thomas Meyer <mnemotron@gmail.com>
2022-02-15 15:04:52 +0100

    Chart display okay

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 function initChart() {
     const ctx = document.getElementById('myChart').getContext('2d');
+    var calculatedData = [];
+    for(var i = 0; i <= 80; i++) {
+        var xVal = (i / 2) - 20;
+        var yVal = 60 - i;
+        calculatedData.push( {
+            x: xVal,
+            y: yVal
+        });
+    }
     const myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                label: 'RL-Temperatur',
+                data: calculatedData,
+                parsing: {
+                    xAxisKey: "x",
+                    yAxisKey: "y",
+                },
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    '#ff0000'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    '#e00000'
                 ],
                 borderWidth: 1
             }]
         },
         options: {
             scales: {
+                x: {
+                    type: "linear",
+                    suggestedMin: -20,
+                    suggestedMax: 20
+                },
                 y: {
-                    beginAtZero: true
+                    type: "linear",
+                    suggestedMin: 0,
+                    suggestedMax: 80
                 }
             }
         }
