ced41296d6bd6556528980400a05ae1d018f20d6
Thomas Meyer <mnemotron@gmail.com>
2022-02-15 17:22:47 +0100

    Split Init and Update. Use update to change data.

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-function initChart() {
-    const calculatedData = [];
-    for(var i = 0; i <= 80; i++) {
-        var xVal = (i / 2) - 20;
-        var yVal = 60 - i;
-        calculatedData.push( {
-            x: xVal,
-            y: yVal
-        });
-    }
+let myChart = null;
 
+function initChart() {
     const ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(ctx, {
+    myChart = new Chart(ctx, {
         type: 'line',
         data: {
             datasets: [{
                 label: 'RL-Temperatur',
-                data: calculatedData,
+                data: [],
                 parsing: {
                     xAxisKey: "x",
                     yAxisKey: "y",
@@ -33,15 +25,47 @@ function initChart() {
             scales: {
                 x: {
                     type: "linear",
+                    title: "Außentemp.",
                     suggestedMin: -20,
                     suggestedMax: 20
                 },
                 y: {
                     type: "linear",
-                    suggestedMin: 0,
-                    suggestedMax: 80
+                    title: "°C",
+                    suggestedMin: 10,
+                    suggestedMax: 40
                 }
             }
         }
     });
+}
+
+function updateChart(heatingEnd, heatingFoot, heatingDelta) {
+    const heatingStep = Math.abs(heatingEnd - heatingFoot) / 40;
+    console.log('heatingEnd=' + heatingEnd + ' heatingFoot=' + heatingFoot + ' heatingStep=' + heatingStep);
+
+    const calculatedData = [];
+ 
+    for(var i = 0; i <= 80; i++) {
+        var xVal = (i / 2) - 20;
+        var yVal = heatingEnd - (i / 2) * heatingStep;
+        calculatedData.push({
+            x: xVal,
+            y: yVal
+        });
+    }
+
+    myChart.data.labels.pop();
+    myChart.data.datasets.forEach((dataset) => {
+        console.log('Pop dataset=' + dataset.data.label + ' data: ' + dataset.data.length);
+        dataset.data.pop();
+    });
+    //myChart.data.labels.push(label);
+    //myChart.data.datasets[0].data.push(calculatedData);
+    myChart.data.datasets[0].data = calculatedData;
+    /*myChart.data.datasets.forEach((dataset) => {
+        console.log('Push set=' + dataset.data.label + ' data: ' + calculatedData.length);
+        dataset.data.push(calculatedData);
+    });*/
+    myChart.update();
 }
